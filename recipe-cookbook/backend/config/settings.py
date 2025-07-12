898cd5fcc6880e0e10341824ba361ef35d9cb4f5
SynthSlav <ljupcebozinovski@gmail.com>
2025-07-12 07:09:15 +0200

    attempt at building frontent for deploy

--- recipe-cookbook/backend/config/settings.py
+++ recipe-cookbook/backend/config/settings.py
@@ -13,21 +13,24 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import dj_database_url
 import os
+import sys
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Check if env.py exists and import it to set environment variables
 # This is useful for local development to avoid hardcoding sensitive information.
-if os.path.isfile("env.py"):
-    import env
+ENV_PATH = os.path.join(BASE_DIR, "env.py")
+if os.path.exists(ENV_PATH):
+    exec(open(ENV_PATH).read())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-8cq)qfjnqb-d5^6yp&4k0j%r!=22ddm1)d!g!b@_5(r#s%cjyh"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -36,6 +39,8 @@ ALLOWED_HOSTS = [
     ".herokuapp.com",
     "127.0.0.1",
 ]
+
+PROJECT_PATH = "recipe-cookbook/backend"
 
 
 # Application definition
@@ -127,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
